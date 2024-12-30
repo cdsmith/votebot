fd18fcd86c9a884652cf7a9dd77c35579cbb854d
Chris Smith <cdsmith@gmail.com>
2024-12-29 23:36:39 -0700

    Fix tests

--- tests/test_tidemanalt.py
+++ tests/test_tidemanalt.py
@@ -5,7 +5,7 @@ from testutil import PrefillBallot
 def test_tideman_alternative_simple_majority():
     # Alice wins immediately with a majority
     election = TidemanAlternativeElection(
-        "", "", candidates=["Alice", "Bob", "Charlie"]
+        "", "", candidates=["Alice", "Bob", "Charlie"], method_params={}
     )
     winners, _ = election.tabulate(
         [
@@ -20,7 +20,7 @@ def test_tideman_alternative_simple_majority():
 def test_tideman_alternative_condorcet_winner():
     # No immediate majority; but Alice is the Condorcet winner.
     election = TidemanAlternativeElection(
-        "", "", candidates=["Alice", "Bob", "Charlie"]
+        "", "", candidates=["Alice", "Bob", "Charlie"], method_params={}
     )
     winners, _ = election.tabulate(
         [
@@ -37,7 +37,7 @@ def test_tideman_alternative_condorcet_winner():
 def test_tideman_alternative_condorcet_cycle():
     # Three-way Condorcet cycle, but Charlie has fewest 1st place votes.  Alice > Bob.
     election = TidemanAlternativeElection(
-        "", "", candidates=["Alice", "Bob", "Charlie"]
+        "", "", candidates=["Alice", "Bob", "Charlie"], method_params={}
     )
     winners, _ = election.tabulate(
         4 * [PrefillBallot(ranking=["Alice", "Bob", "Charlie"])]
@@ -50,7 +50,7 @@ def test_tideman_alternative_condorcet_cycle():
 def test_tideman_alternative_condorcet_cycle_after_elim():
     # Doug is a Condorcet loser who needs to be eliminated first.
     election = TidemanAlternativeElection(
-        "", "", candidates=["Alice", "Bob", "Charlie", "Doug"]
+        "", "", candidates=["Alice", "Bob", "Charlie", "Doug"], method_params={}
     )
     winners, _ = election.tabulate(
         4 * [PrefillBallot(ranking=["Doug", "Alice", "Bob", "Charlie"])]
